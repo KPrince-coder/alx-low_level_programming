cf78203206a42f60a5682ff4b71be0cb85b88a27
Kyeremeh Prince <kyeremehprince68@gmail.com>
2022-07-04 16:41:13 +0000

    fix error with code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,17 +8,11 @@
 
 int main(void)
 {
-	int intSize;
-	float floatSize;
-	char charSize;
-	long long int longLongSize;
-	long int longSize;
-
 	/*printing the sizes of the various data types*/
-	printf("Size of a char: %zu byte(s)\n", (unsigned long)sizeof(charSize));
-	printf("Size of an int: %zu byte(s)\n", (unsigned long)sizeof(intSize));
-	printf("Size of a long int: %zu byte(s)\n", (unsigned long)sizeof(longSize));
-	printf("Size of a long long int: %zu bytes(s)\n", (unsigned long)sizeof(longLongSize));
-	printf("Size of a float: %zu bytes(s)\n", (unsigned long)sizeof(floatSize));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu bytes(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu bytes(s)\n", sizeof(float));
 	return (0);
 }
