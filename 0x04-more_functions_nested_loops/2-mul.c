974daf5b94a12a0bcba17eb6a1e5143fc1545db4
Kyeremeh Prince <kyeremehprince68@gmail.com>
2022-07-08 05:41:37 +0000

    multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: integer
+ * @b: integer
+ *
+ * Return: result of the multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 Projects on C - more functions, more nested loops
 0.Functions checks for uppercase character.
 1.Checks for a digit.
+2.Function multiplies two integers.
